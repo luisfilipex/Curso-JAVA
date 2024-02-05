5e4099ba912712cff9c6ec4a8f2e0e45209470f5
Luis Filipe e Silva <137459500+luisfilipex@users.noreply.github.com>
2024-02-05 14:05:44 -0300

    Banco digital
    Desafio banco digital feito pela dio com orientação a objetos

---
+++ JAVA-DIO/BancoDigital/src/BancoDigital/ContaPoupanca.java
@@ -0,0 +1,18 @@
+package BancoDigital;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    public ContaPoupanca() {  //construtor sugerido pela IDE
+        super();
+    }
+
+    public void imprimirExtrato() {
+        System.out.println("#### Extrato de Conta Poupança #####");
+        super.imprimirInfosComuns();
+    }
+
+}
