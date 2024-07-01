daa9ca827761464f2bb9fb8de3be2da0071538bc
Luis Filipe e Silva <137459500+luisfilipex@users.noreply.github.com>
2024-07-01 19:03:53 -0300

    Add files via upload

---
+++ JAVA-DIO/execptionAula - Copia/src/ExeceptionCustomizada1.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.io.*;
+
+class ImpossivelAberturaDeArquivoException extends Exception {
+
+    private String nomeDoArquivo;
+    private String diretorio;
+
+    public ImpossivelAberturaDeArquivoException(String nomeDoArquivo, String diretorio) {
+        super("O arquivo " + nomeDoArquivo + " não foi encontrado no diretório " + diretorio);
+        this.nomeDoArquivo = nomeDoArquivo;
+        this.diretorio = diretorio;
+    }
+
+    /*@Override
+    public String toString() {
+        return "ImpossivelAberturaDeArquivoException{" +
+                "nomeDoArquivo='" + nomeDoArquivo + '\'' +
+                ", diretorio='" + diretorio + '\'' +
+                '}';
+    }*/
+
+}
+
+
